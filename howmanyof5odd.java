41bbd87bc705587d27558f6d0286a7df54f00a05
sangeetasingh25 <jeeaspirantgoi@gmail.com>
2026-02-01 23:05:33 +0530

    Add files via upload

---
+++ howmanyof5odd.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+   public class HowManyAreOdd{
+       public static void main(String[] args){
+          Scanner sc = new Scanner(System.in);
+          int input1 = sc.nextInt();
+          int input2 = sc.nextInt();
+          int input3 = sc.nextInt();
+          int input4 = sc.nextInt();
+          int input5 = sc.nextInt();
+          sc.close();
+          int count = 0;
+          if(input1%2!=0){
+            count++;
+          }
+          if(input2%2!=0){
+             count++;
+          }
+          if(input3%2!=0){
+             count++;
+          }
+          if(input4%2!=0){
+             count++;
+          }
+          if(input5%2!=0){
+             count++;
+          }
+               System.out.println(count);
+  }
+}
