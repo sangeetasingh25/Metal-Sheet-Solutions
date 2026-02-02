b8e9f2a1bd00394cb88bb9103ad11ce57249306e
sangeetasingh25 <jeeaspirantgoi@gmail.com>
2026-02-02 20:43:50 +0530

    Add files via upload

---
+++ isPrime.java
@@ -0,0 +1,19 @@
+mport java.util.Scanner;
+  public class Isprime{
+     public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+         int number = sc.nextInt();
+           int count=0;
+       for(int i=1;i<=number;i++){
+            if(number%i==0){
+               count++;
+            }
+       }
+        if(count==2){
+           System.out.println("2");
+        }
+       else{
+         System.out.println("1");
+       }
+ }
+}
